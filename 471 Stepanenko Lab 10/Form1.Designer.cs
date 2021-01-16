0321c2d6388fb1ed383903754a4c12280fd9823d
unknown <Nikolaua36@gmail.com>
2021-01-16 18:12:46 +0200

    finish

---
+++ 471 Stepanenko Lab 10/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace _471_Stepanenko_Lab_10
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.deleteAll = new System.Windows.Forms.Button();
+            this.deleteOne = new System.Windows.Forms.Button();
+            this.addButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 19;
+            this.listBox1.Location = new System.Drawing.Point(52, 121);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(359, 213);
+            this.listBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(52, 370);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(359, 26);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Зчитати записи з БД";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // deleteAll
+            // 
+            this.deleteAll.Location = new System.Drawing.Point(52, 402);
+            this.deleteAll.Name = "deleteAll";
+            this.deleteAll.Size = new System.Drawing.Size(359, 26);
+            this.deleteAll.TabIndex = 3;
+            this.deleteAll.Text = "Видалити всі записи з БД";
+            this.deleteAll.UseVisualStyleBackColor = true;
+            this.deleteAll.Click += new System.EventHandler(this.deleteAll_Click);
+            // 
+            // deleteOne
+            // 
+            this.deleteOne.Location = new System.Drawing.Point(52, 434);
+            this.deleteOne.Name = "deleteOne";
+            this.deleteOne.Size = new System.Drawing.Size(359, 26);
+            this.deleteOne.TabIndex = 4;
+            this.deleteOne.Text = "Видалити виділений елемент";
+            this.deleteOne.UseVisualStyleBackColor = true;
+            this.deleteOne.Click += new System.EventHandler(this.deleteOne_Click);
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(693, 153);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(359, 26);
+            this.addButton.TabIndex = 5;
+            this.addButton.Text = "Додати";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(52, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(268, 35);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Робота з базою даних";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(693, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(171, 35);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Додати запис";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(693, 121);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.PlaceholderText = "Додати запис";
+            this.textBox1.Size = new System.Drawing.Size(359, 26);
+            this.textBox1.TabIndex = 8;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(52, 523);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(359, 26);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Обрати лише технічні предмети";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1139, 718);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.deleteOne);
+            this.Controls.Add(this.deleteAll);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "471 Степаненко М.І. Лаба 10-14";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button deleteAll;
+        private System.Windows.Forms.Button deleteOne;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ 471 Stepanenko Lab 10/Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace _471_Stepanenko_Lab_10
+{
+    public partial class Form1 : Form
+    {
+        public static string connectString = String.Format("Provider=Microsoft.Jet.OLEDB.4.0;Data Source={0}", @"C:\Users\USER\source\repos\471 Stepanenko Lab 10\471 Stepanenko Lab 10\database.mdb");
+        // вариант 2
+        //public static string connectString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=database.mdb;";
+
+        // поле - ссылка на экземпляр класса OleDbConnection для соединения с БД
+        OleDbConnection myConnection;
+        public Form1()
+        {
+            InitializeComponent();
+            myConnection = new OleDbConnection(connectString);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            GetData();
+        }
+
+        private void deleteAll_Click(object sender, EventArgs e)
+        {
+            DeleteAll();
+            GetData();
+        }
+
+        private void deleteOne_Click(object sender, EventArgs e)
+        {
+            DeleteOne();
+            GetData();
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            PushData();
+            GetData();
+        }
+
+        private void GetData()
+        {
+            using (OleDbConnection myConnection = new OleDbConnection(connectString))
+            {
+                try
+                {
+                    myConnection.Open();
+                    string query = "SELECT * from Subjects";
+                    OleDbCommand command = new OleDbCommand(query, myConnection);
+                    OleDbDataReader reader = command.ExecuteReader();
+
+                    //label1.Text = command.ExecuteScalar().ToString();
+                    listBox1.Items.Clear();
+                    while (reader.Read())
+                    {
+                        listBox1.Items.Add(String.Format("{0} {1}", reader[0].ToString(), reader[1].ToString()));
+                    }
+                    reader.Close();
+                    myConnection.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+        private void PushData()
+        {
+            using (OleDbConnection myConnection = new OleDbConnection(connectString))
+            {
+                try
+                {
+                    myConnection.Open();
+                    string value = textBox1.Text;
+                    if (value == "")
+                    {
+                        MessageBox.Show("Ви не ввели значення запису", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    else
+                    {
+                        string query = String.Format("insert into Subjects (subject_name) values (\"{0}\")", value);
+                        OleDbCommand command = new OleDbCommand(query, myConnection);
+                        command.ExecuteNonQuery();
+                    }
+                    
+                    myConnection.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private void DeleteOne()
+        {
+            using (OleDbConnection myConnection = new OleDbConnection(connectString))
+            {
+                try
+                {
+                    myConnection.Open();
+                    string[] index = listBox1.SelectedItem.ToString().Split(new char[] { ' ', '\n' });
+                    string query = String.Format("DELETE FROM Subjects WHERE Код = {0}", index[0]);
+
+                    // создаем объект OleDbCommand для выполнения запроса к БД MS Access
+                    OleDbCommand command = new OleDbCommand(query, myConnection);
+
+                    // выполняем запрос к MS Access
+                    command.ExecuteNonQuery();
+                    myConnection.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+        private void DeleteAll(){
+            using (OleDbConnection myConnection = new OleDbConnection(connectString))
+            {
+                try
+                {
+                    myConnection.Open();
+                    string query = "DELETE FROM Subjects";
+
+                    // создаем объект OleDbCommand для выполнения запроса к БД MS Access
+                    OleDbCommand command = new OleDbCommand(query, myConnection);
+
+                    // выполняем запрос к MS Access
+                    command.ExecuteNonQuery();
+                    myConnection.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //лише технічні предмети
+            using (OleDbConnection myConnection = new OleDbConnection(connectString))
+            {
+                try
+                {
+                    myConnection.Open();
+                    string query = "SELECT * from Subjects where type = \"Технічна\"";
+                    OleDbCommand command = new OleDbCommand(query, myConnection);
+                    OleDbDataReader reader = command.ExecuteReader();
+
+                    //label1.Text = command.ExecuteScalar().ToString();
+                    listBox1.Items.Clear();
+                    while (reader.Read())
+                    {
+                        listBox1.Items.Add(String.Format("{0} {1}", reader[0].ToString(), reader[1].ToString()));
+                    }
+                    reader.Close();
+                    myConnection.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+    }
+}
